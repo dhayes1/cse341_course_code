55429298e9a8d5ec5ca1215496dec3020347d816
dhayes1 <donny.hayes@gmail.com>
2023-01-28 12:29:55 -0800

    Add API documentation

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connection');
+// const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+app
+  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app
   .use(bodyParser.json())
