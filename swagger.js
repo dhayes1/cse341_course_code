38b14794b6cc5b7f9b57dec7b73bab0e7b19c148
dhayes1 <donny.hayes@gmail.com>
2023-01-28 15:42:03 -0800

    Update swagger to work with Render

--- swagger.js
+++ swagger.js
@@ -5,15 +5,11 @@ const doc = {
     title: 'CSE 341 Contacts API',
     description: 'API for interacting with the CSE 341 contacts database.',
   },
-  host: 'localhost:8080',
+  host: 'https://dhayes-cse341-course-code-ogxh.onrender.com',
   schemes: ['http'],
 };
 
 const outputFile = './swagger-output.json';
 const endpointsFiles = ['./routes/index.js'];
-
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
 
 swaggerAutogen(outputFile, endpointsFiles, doc);
