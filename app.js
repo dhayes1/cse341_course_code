1aac7f359ba7812e2b4d9b63bc31737383b70e33
dhayes1 <donny.hayes@gmail.com>
2023-01-28 15:44:49 -0800

    Update Swagger to work with Render

--- app.js
+++ app.js
@@ -3,16 +3,11 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connection');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
-const cors = require('cors');
+
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-app
-  .use(cors({
-    origin: '*'
-  }));
-  
 app
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
@@ -20,6 +15,12 @@ app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allowe-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     next();
   })
   .use('/', require('./routes'));
