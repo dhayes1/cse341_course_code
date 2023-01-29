fefae7dd4cec5595c651667a454826db014abd58
dhayes1 <donny.hayes@gmail.com>
2023-01-28 19:08:23 -0800

    Add error handling

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,30 +4,38 @@ const ObjectId = require('mongodb').ObjectId;
 const getAll = async (req, res, next) => {
   // #swagger.tags = ['contacts']
   // #swagger.summary = 'Finds all contacts'
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+  try {
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .find();
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const getSingle = async (req, res, next) => {
   // #swagger.tags = ['contacts']
   // #swagger.summary = 'Finds a single contact by ID'
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .find({ _id: userId });
+    result.toArray().then((lists) => {
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const createContact = async (req, res) => {
@@ -45,22 +53,26 @@ const createContact = async (req, res) => {
           about: ''
         }
   } */
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .insertOne(contact);
-  if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
-    res.status(500).json(response.error || 'ERROR: Cannot create contact.');
+  try {
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .insertOne(contact);
+    if (response.acknowledged) {
+      res.status(201).json(response);
+    } else {
+      res.status(500).json(response.error || 'ERROR: Cannot create contact.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
@@ -80,39 +92,47 @@ const updateContact = async (req, res, next) => {
             about: ''
         }
   } */
-  const contactId = new ObjectId(req.params.id);
-  const contact = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday
-  };
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .updateOne({ _id: contactId }, { $set: contact });
-  if (response.modifiedCount > 0) {
-    res.status(204).json(response);
-  } else {
-    res.status(500).json(response.error || 'ERROR: Cannot update contact.');
+  try {
+    const contactId = new ObjectId(req.params.id);
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .updateOne({ _id: contactId }, { $set: contact });
+    if (response.modifiedCount > 0) {
+      res.status(204).json(response);
+    } else {
+      res.status(500).json(response.error || 'ERROR: Cannot update contact.');
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
 const deleteContact = async (req, res, next) => {
   // #swagger.tags = ['contacts']
   // #swagger.summary = 'Delete a contact by ID'
-  const contactId = new ObjectId(req.params.id);
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection('contacts')
-    .deleteOne({ _id: contactId });
-  if (response.deletedCount > 0) {
-    res.status(200).json(response);
-  } else {
-    res.status(500).json(response.error || `ERROR: Cannot delete contact. ${contactId}`);
+  try {
+    const contactId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection('contacts')
+      .deleteOne({ _id: contactId });
+    if (response.deletedCount > 0) {
+      res.status(200).json(response);
+    } else {
+      res.status(500).json(response.error || `ERROR: Cannot delete contact. ${contactId}`);
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
